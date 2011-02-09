fc1129cc459ee0851610fa987d56c3293cedb49e
Urs Graf <urs.graf@ntb.ch>
2011-02-09 06:30:53 +0000

    initial import

---
+++ src/java/lang/BString.java
@@ -0,0 +1,13 @@
+package java.lang;
+
+import ch.ntb.inf.deep.runtime.mpc555.Heap;
+import ch.ntb.inf.deep.unsafe.US;
+
+/* changes:
+ * 11.11.10	NTB/Urs Graf	creation
+ */
+public class BString {
+	/** count is the number of characters in the string. */
+	protected int count;
+	
+}
