b182ea1b63689cae4a731e11528b2a46e708c084
Urs Graf <urs.graf@ntb.ch>
2011-02-25 13:16:21 +0000

    initial import

---
+++ src/java/lang/Long.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+/* changes:
+ * 25.02.11 NTB/Urs Graf		creation
+ */
+public class Long {
+
+	public final static long MAX_VALUE = 0x7fffffffffffffffL;
+	public final static long MIN_VALUE = 0x8000000000000000L;
+	
+}
