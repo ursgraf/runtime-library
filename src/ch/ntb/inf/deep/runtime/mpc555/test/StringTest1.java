78f2af7e23d7e6ca6d36d6aef5f4e6cadc549ed9
Urs Graf <urs.graf@ntb.ch>
2011-03-25 14:52:30 +0000

    string test improved

--- src/ch/ntb/inf/deep/runtime/mpc555/test/StringTest1.java
+++ src/ch/ntb/inf/deep/runtime/mpc555/test/StringTest1.java
@@ -9,7 +9,9 @@ import ch.ntb.inf.deep.runtime.mpc555.driver.SCI2;
  */
 
 public class StringTest1 {
-	static String str1;
+	static String str1, str2;
+	
+	
 	static {
 		SCI2.start(9600, SCI2.NO_PARITY, (short)8);
 		System.out = new PrintStream(SCI2.out);
@@ -20,6 +22,11 @@ public class StringTest1 {
 		char[] a1 = new char[] {'1', '2', '3', '4', '5', '6'};
 		str1 = new String(a1, 2, 3);
 		System.out.println(str1);
+		str2 = new String(a1, 0, 6);
+		char[] a2 = new char[str2.length()];
+		for (int i = 0; i < str2.length(); i++) a2[i] = str2.charAt(i);
+		a2[0] = str2.charAt(0);
+		System.out.println(str2);
 		System.out.println("done");
 	}
 }
