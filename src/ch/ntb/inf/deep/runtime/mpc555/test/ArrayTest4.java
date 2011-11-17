b94c52066055605bed7f35a129e7a14f651c6e74
Martin Zueger <martin.zueger@ntb.ch>
2011-11-17 15:20:16 +0000

    ArrayTest4 added

---
+++ src/ch/ntb/inf/deep/runtime/mpc555/test/ArrayTest4.java
@@ -0,0 +1,17 @@
+package ch.ntb.inf.deep.runtime.mpc555.test;
+
+public class ArrayTest4 {
+
+	static {
+		Object a1 = new A[3];
+		if(a1 instanceof A[]){
+			
+		}
+		
+		Object b1 = new B[3];
+
+	}
+}
+
+class A{}
+class B{}
