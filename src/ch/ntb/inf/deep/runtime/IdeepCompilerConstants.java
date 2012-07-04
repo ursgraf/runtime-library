22bd317b417cfc7daa7c8f448dcf55be04b66009
Martin Zueger <martin.zueger@ntb.ch>
2012-07-04 09:21:26 +0000

    IdeepCompilerConstants added

---
+++ src/ch/ntb/inf/deep/runtime/IdeepCompilerConstants.java
@@ -0,0 +1,16 @@
+package ch.ntb.inf.deep.runtime;
+
+public interface IdeepCompilerConstants {
+
+	// Compiler specific constants
+	public static final int cblkCodeBaseOffset = 0x4;
+	public static final int cblkCodeSizeOffset = 0x8;
+	public static final int cblkVarBaseOffset = 0xc;
+	public static final int cblkVarSizeOffset = 0x10;
+	public static final int cblkClinitAddrOffset = 0x14;
+	public static final int cblkNofPtrsOffset = 0x18;
+	public static final int stStackOffset = 0x4;
+	public static final int stHeapOffset = 0x8;
+	public static final int stKernelClinitAddr = 0xc;
+
+}
